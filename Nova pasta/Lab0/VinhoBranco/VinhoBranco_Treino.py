f98c041378a49d24a5fcc1ff9db5cf5ee56d3eae
aniltonlc <aniltonconceicao63@gmail.com>
2026-03-27 12:02:59 +0000

    Repositorio alterado

---
+++ Nova pasta/Lab0/VinhoBranco/VinhoBranco_Treino.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import pickle as p1
+from sklearn import linear_model
+
+data = pd.read_csv("C:/Users/anilt/Documents/MATÉRIAS_2_SEMESTRE/Aprendizagem Automática/winequality-white.csv",
+sep=";")
+
+train_data=data[:1000]
+data_X=train_data.iloc[:,0:11]
+data_Y=train_data.iloc[:,11:12]
+#print(train_data.columns)
+print(data_X)
+print(data_Y)
+regr = linear_model.LinearRegression()
+preditor_linear_model=regr.fit(data_X, data_Y)
+preditor_Pickle = open('./white-wine_quality_predictor', 'wb')
+print("white-wine_quality_predictor")
+p1.dump(preditor_linear_model, preditor_Pickle)
+
+
+
