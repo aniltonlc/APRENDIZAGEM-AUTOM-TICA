0a0832a9b3e44b31ea4fd43f0f0e7ba9fd4e9ecd
aniltonlc <aniltonconceicao63@gmail.com>
2026-03-27 12:38:04 +0000

    Update VinhoBranco_Previsão.py

--- Nova pasta/Lab0/VinhoBranco/VinhoBranco_Previsão.py
+++ Nova pasta/Lab0/VinhoBranco/VinhoBranco_Previsão.py
@@ -1,18 +1,25 @@
-import pandas as pd
+import matplotlib.pyplot as plt
 import pickle as p1
-from sklearn import linear_model
+import numpy as np
+import pandas as pd
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
-data = pd.read_csv("C:/Users/anilt/Documents/MATÉRIAS_2_SEMESTRE/Aprendizagem Automática/winequality-white.csv",
-sep=";")
 
-right=0
-wrong=0
-total=0
-for x in z_pred["quality"]:
-    z=int(x)
-    total=total+1
-    if z==0:
-        right=right+1
-    else:
-        wrong=wrong+1
-    print("accuraccy1= ",right/total,"accuraccy2= ",wrong/total)
+data_x=input("introduza valores do wine\n")
+data=data_x.split(";")
+print(data)
+fmap_data = map(float, data)
+print(fmap_data)
+flist_data = list(fmap_data)
+print(flist_data)
+data1 = pd.read_csv("./winequality-white.csv",sep=";")
+data2=data1.iloc[:0,:11]
+data_preparation=pd.DataFrame([flist_data],columns=list(data2))
+out=data2
+for x in out:
+    print(x,data_preparation[x].values)
+loaded_model = p1.load(open('./white-wine_quality_predictor', 'rb'))
+y_pred=loaded_model.predict(data_preparation)
+
+print("wine quality",int(y_pred[0,0]))
